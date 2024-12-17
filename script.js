a35406049415e1c78679972b791ef55e337d5ac5
shuo xu <shuo.xu@2021.icam.fr>
2024-12-17 09:41:12 +0100

    JS: letter press event

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const words = ['application', 'programming', 'interface', 'wizard'];
 
 // get a random word from the 'words' array
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-console.log(selectedWord);
+// console.log(selectedWord);
 
 const correctLetters = [];
 const wrongLetters = [];
 
-// show hidden word: split the selectedWord string into an array and loop through each letter, if the current letter we looping through has been found in the correctLetters array, then output the letter, or output an empty '', and finally we turn the array back into a string
+// show hidden word: split the 'selectedWord' string into an array and loop through each letter, if the current letter we looping through has been found in the 'correctLetters' array, then output the letter, or output an empty '', and finally we turn the array back into a string
 function displayWord() {
     wordEl.innerHTML = `
     ${selectedWord.split('').map(letter =>
@@ -35,5 +35,62 @@ function displayWord() {
         popup.style.display = 'flex';
     }
 }
+
+
+
+
+
+
+// update the wrong letters 
+function updateWrongLettersEl() {
+    console.log('update wrong');
+}
+
+
+
+
+
+
+// show notification
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, "2000");
+}
+
+
+
+
+// keydown letter press
+window.addEventListener('keydown', e => {
+    // 65 < e.keyCode < 90 are letters 
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        // put the hitten letter into the 'letter' variable
+        const letter = e.key; 
+        // console.log(letter);
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            } 
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+        }
+    } 
+});
+
+
 
 displayWord()
